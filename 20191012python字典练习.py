a33f1279cdc6e547ed78478fe9d31ec55332a1ba
ruopu89 <39111641+ruopu89@users.noreply.github.com>
2020-12-18 15:11:13 +0800

    20201218pycharmtest

--- 20191012python字典练习.py
+++ 20191012python字典练习.py
@@ -3,149 +3,231 @@
 # names = ['Alice','Beth','Cecil','Dee-Dee','Earl']
 # numbers = ['2314','9191','23423','1230','3333']
 # print(numbers[names.index('Cecil')])
-
+# 输出： 23423
+# ===============================================
 # 初始化/定义
 
 # phonebook = {'Alice':1234,'Beth':'90129','Cecil':'123456'}
 # print(phonebook['Alice'])
-
+# 输出：1234
+# =========================================================
 # l1 = list(range(5))
-# # d = dict(l1)
-# # print(d)
-# # 问题是l1是一个序列，但不是二元组。
+# d = dict(l1)
+# print(d)
+# 输出：TypeError: cannot convert dictionary update sequence element #0 to a sequence
+# 问题是l1是一个序列，但不是二元组。
+
 # d = dict(enumerate(l1))
 # print(d)
+# 输出：{0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
 
 # e = enumerate(l1)
 # print(type(e))
 # for x in e:
 #     print(x)
+# 输出：
+# <class 'enumerate'>
+# (0, 0)
+# (1, 1)
+# (2, 2)
+# (3, 3)
+# (4, 4)
 
 # f = dict(e)
 # print(f)
+# 输出：
+# {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
 
 # e = enumerate(l1)
 # f = dict(e)
 # print(f)
-
+# 输出：
+# {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
+# ==========================================================
 # g = {('a',1)}
 # print(g)
-
+# 输出：{('a', 1)}
+# ===========================================================
 # h = dict(((1,'a'),))
 # print(h)
-
+# 输出：{1: 'a'}
+# ===============================================================
 # items = [('name','gumby'),('age',32)]
 # a = dict(items)
 # print(a)
 # print(a['name'])
-# # 调用字典对象的方法的时候，键一定要有单引号？
+# 输出：
+# {'name': 'gumby', 'age': 32}
+# gumby
+# 调用字典对象的方法的时候，键一定要有单引号
+# ======================================================================
 
 # l = dict(name='abc',age=21)
 # print(l['name'])
+# 输出：abc
+# =========================================================================
 
 # **循环遍历所有的key**
 dict = {'k1':'v1','k2':'v2','k3':'v3'}
-for k in dict:
-    print(k)
+# for k in dict:
+#     print(k)
+# 输出:
+# k1
+# k2
+# k3
 
-for k in dict.keys():
-    print(k)
+# for k in dict.keys():
+#     print(k)
+# 输出:
+# k1
+# k2
+# k3
 
 # **循环遍历所有value**
-for k in dict:
-    print(dict[k])
+# for k in dict:
+#     print(dict[k])
+# 输出:
+# v1
+# v2
+# v3
 
-for k in dict.keys():
-    print(dict.get(k))
+# for k in dict.keys():
+#     print(dict.get(k))
+# 输出:
+# v1
+# v2
+# v3
 
-for v in dict.values():
-    print(v)
+
+# for v in dict.values():
+#     print(v)
+#  输出:
+# v1
+# v2
+# v3
 
 # **循环遍历出所有的key和value**
-for k in dict:
-    print(k,dict[k])
+# for k in dict:
+#     print(k,dict[k])
+# 输出:
+# k1 v1
+# k2 v2
+# k3 v3
 
-for k,v in dict.items():
-    print(k,v)
+# for k,v in dict.items():
+#     print(k,v)
+# 输出:
+# k1 v1
+# k2 v2
+# k3 v3
+#
+# for item in dict.items():
+#     print(item)
+# 输出:
+# ('k1', 'v1')
+# ('k2', 'v2')
+# ('k3', 'v3')
+#
+# for item in dict.items():
+#     print(item[0],item[1])
+# 输出:
+# k1 v1
+# k2 v2
+# k3 v3
 
-for item in dict.items():
-    print(item)
+# for k,_ in dict.items():
+#     print(k)
+# 输出:
+# k1
+# k2
+# k3
 
-for item in dict.items():
-    print(item[0],item[1])
-
-for k,_ in dict.items():
-    print(k)
-
-for _,v in dict.items():
-    print(v)
+# for _,v in dict.items():
+#     print(v)
+# 输出:
+# v1
+# v2
+# v3
 
 # 请在字典中增加一个键值对,"k4":"v4"，输出添加后的字典
-dict['k4'] = 'v4'
-print(dict)
+# dict['k4'] = 'v4'
+# print(dict)
+# 输出: {'k1': 'v1', 'k2': 'v2', 'k3': 'v3', 'k4': 'v4'}
 
 # 请删除字典中键值对"k1":"v1",并输出删除后的结果
-dict.pop("k1")
+# dict.pop("k1")
 # key存在，移除它，并返回它的value;key不存在，返回给定的default;default未设置，key不存在则抛出KeyError异常
-print(dict)
+# print(dict)
+# 输出: {'k2': 'v2', 'k3': 'v3'}  # 因为执行时注释了上面的k4 = v4,所以这里没有k4
 
-del dict['k2']
-print(dict)
+# del dict['k2']
+# print(dict)
+# 输出: {'k1': 'v1', 'k3': 'v3'}
 # del dict['k2']看着像删除了一个对象，本质上减少了一个对象的引用，del实际上删除的是名称，而不是对象
 
 # 请删除字典中键"k5"对应的键值对，如果字典中不存在键"k5",则不报错，返回None
-w = dict.pop('k5',None)
-print(w)
+# w = dict.pop('k5',None)
+# print(w)
+# 输出: None
 
-print("删除不存在的k5,不报错，返回值: ",dict.pop("k5",None))
+# print("删除不存在的k5,不报错，返回值: ",dict.pop("k5",None))
 
 # 请获取字典中"k3"对应的值
-print(dict['k3'])
+# print(dict['k3'])
+# 输出: v3
 
 # 请获取字典中"k6"对应的值,如果不存在，则不报错，并且让其返回None。
-print(dict.get('k6',None))
+# print(dict.get('k6',None))
+# 输出: None
+# ===============================================================================================
 
 # 现有dict2 = {"k1":"v11","a":"b"},通过一行操作使dict2 = {"k1":"v1","k2":"v2","k3":"v3","a":"b"}
-dict = {"k1":"v1","k2":"v2","k3":"v3"}
-dict2 = {"k1":"v11","a":"b"}
-dict2.update(dict)
-print("dict: ",dict)
-print("dict2: ",dict2)
+# dict = {"k1":"v1","k2":"v2","k3":"v3"}
+# dict2 = {"k1":"v11","a":"b"}
+# dict2.update(dict)
+# print("dict: ",dict)
+# print("dict2: ",dict2)
+# 输出:
+# dict:  {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'}
+# dict2:  {'k1': 'v1', 'a': 'b', 'k2': 'v2', 'k3': 'v3'}
+# ========================================================================================================
 
 # 组合嵌套题。写代码，有如下列表，按照要求实现每一个功能
-lis = [["k",["qwe",20,{"k1":["tt",3,"1"]},89],"ab"]]
+# lis = [["k",["qwe",20,{"k1":["tt",3,"1"]},89],"ab"]]
 # 10.1、将列表中的"tt"改为大写(用两种方法)
 # 10.2、将列表中的字符串"1"变成数字101(用两种方法)
 # 10.3、将数字3变成字符串'100'(用两种方法)
 
 # 10.1 方法1:
-print(lis[0][1][2].get("k1")[0].upper())
+# print(lis[0][1][2].get("k1")[0].upper())
 # 10.1 方法2:
-print(lis[0][1][2].get("k1")[0].swapcase())
+# print(lis[0][1][2].get("k1")[0].swapcase())
 
 # 10.2 方法1:
-lis[0][1][2]["k1"][2] = 101
-print(lis)
+# lis[0][1][2]["k1"][2] = 101
+# print(lis)
 # 10.2 方法2:
-lis[0][1][2].get("k1")[2]=101
-print(lis)
+# lis[0][1][2].get("k1")[2]=101
+# print(lis)
 
 # 10.3 方法1：
-lis[0][1][2].get('k1')[1] = '100'
-print(lis)
-# 10.3 方法2：
-lis[0][1][2]['k1'][1] = '100'
-print(lis)
+# lis[0][1][2].get('k1')[1] = '100'
+# print(lis)
+# # 10.3 方法2：
+# lis[0][1][2]['k1'][1] = '100'
+# print(lis)
 
 # 20191015
 # 字典是可变的，无序的，key不重复。item指kv对。字典是唯一的内置映射类型
 
-phonebook = {'Alie':123,'Beth':'1238','Cecil':10923}
-print(phonebook['Cecil'])
-
-del(dict)
+# phonebook = {'Alie':123,'Beth':'1238','Cecil':10923}
+# print(phonebook['Cecil'])
+#
+# del(dict)
 # 如果不加入此行，下面执行时会报TypeError: 'dict' object is not callable。网上有人说原因是dict()是python的一个内建函数，
 # 如果将dict自定义为一个python字典，在之后想调用dict()函数是会报出“TypeError: 'dict' object is not callable”的错误
 # 在jupeter中执行下面的代码并没有问题，在这里单独执行下面代码也没有问题。应该是上面定义过dict，所以这里再次定义会报错
-d = dict(enumerate(list(range(5))))
-print(d)
+# d = dict(enumerate(list(range(5))))
+# print(d)
+# 输出: {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
+
--- 方法与算法.py
+++ 方法与算法.py
@@ -1,4 +1,4 @@
-# 冒泡法
+# 一、冒泡法
 # 冒泡法总结
 # - 冒泡法需要数据一轮轮比较
 # - 可以设定一个标记判断此轮是否有数据交换发生，如果没有发生交换，可以结束排序，如果发生交换，继续下一轮排序
@@ -44,7 +44,7 @@ for i in range(length):
         break
 print(nums, count_swap, count)
 
-# 命名元组 namedtuple
+# 二、命名元组 namedtuple
 # 语法：
     # namedtuple(typename,field_names,verbose=False,rename=False)
     # 命名元组，返回一个元组的子类，并定义了字段
@@ -60,7 +60,7 @@ print(s.name)
 print(s.age)
 isinstance(s,tuple)
 
-# 简单选择排序
+# 三、简单选择排序
   # 属于选择排序
   # 两两比较大小，找出极值（极大值或极小值）被放置在固定的位置，这个固定位置一般指的是某一端
   # 结果分为升序和降序排列
@@ -211,3 +211,156 @@ for i in range(length // 2):
         nums[minorigin],nums[minindex] = nums[minindex],nums[minorigin]
         count_swap += 1
 print(nums,count_swap,count_iter)
+
+
+
+/vmfs/devices/disks/t10.ATA_____INTEL_SSDSC2BB160G4_____________________BTWL320401T2160MGN__
+
+
+# 四、列表解析List Comprehension
+# 语法
+#  - [返回值 for 元素 in 可迭代对象 if 条件]
+#  - 使用中括号[],内部是for循环,if条件语句可选
+#  - 返回一个新的列表
+#- 列表解析式是一种语法糖
+#  - 编译器会优化,不会因为简写而影响效率,反而因优化提高了效率
+#  - 减少程序员工作量,减少出错
+#  - 简化了代码,但可读性增强
+#- 举例
+#  - 获取10以内的偶数,比较执行效率
+
+# [expr for item in iterable if cond1 if cond2]
+# 这里不能使用elif。
+# 等价于
+#ret = []
+#for item in iterable:
+#	if cond1:
+#		if cond2:
+#			ret.append(expr)
+
+# [expr for i in iterable1 for j in iterable2 ]
+# 等价于
+#ret = []
+#for i in iterable1:
+#	for j in iterable2:
+#		ret.append(expr)
+
+练习(要求使用列表解析式完成)
+1. 返回1-10平方的列表
+[x ** 2 for x in range(1,11)]
+
+2. 有一个列表lst = [1,4,9,16,2,5,10,15],生成一个新列表,要求新列表元素是lst相邻2项的和
+lst = [1,4,9,16,2,5,10,15]
+[lst[i]+lst[i+1] for i in range(len(lst)-1)]
+
+3. 打印九九乘法表
+[print('{}*{}={:<3}{}'.format(j,i,i*j,'\n' if i==j else ''),end="") for i in range(1,10) for j in range(1,i+1)]
+# j,i,i*j,'\n' if i==j else ''是一个三目运算
+输出：
+1*1=1  
+1*2=2  2*2=4  
+1*3=3  2*3=6  3*3=9  
+1*4=4  2*4=8  3*4=12 4*4=16 
+1*5=5  2*5=10 3*5=15 4*5=20 5*5=25 
+1*6=6  2*6=12 3*6=18 4*6=24 5*6=30 6*6=36 
+1*7=7  2*7=14 3*7=21 4*7=28 5*7=35 6*7=42 7*7=49 
+1*8=8  2*8=16 3*8=24 4*8=32 5*8=40 6*8=48 7*8=56 8*8=64 
+1*9=9  2*9=18 3*9=27 4*9=36 5*9=45 6*9=54 7*9=63 8*9=72 9*9=81 
+[None,
+...
+ None]
+# 可以看到，标准输出的是None的列表
+
+4. "0001.abadicddws" 是ID格式,要求ID格式是以点号分割,左边是4位从1开始的整数,右边是10位
+随机小写英文字母。请依次生成前100个ID的列表
+import random
+['{:04}.{}'.format(n,''.join([random.choice(bytes(range(97,123)).decode()) for _ in range(10)])) for n in range(1,101)]
+输出：
+['0001.fbyzrdzoif',
+ '0002.loirmqauym',
+ ...
+ '0098.cjbghrvfmh',
+ '0099.xljsgucscx',
+ '0100.nieckficgn']
+# 可以看到这是标准输出的
+
+['{:04}.{}'.format(i,"".join([chr(random.randint(97,122)) for j in range(10)])) for i in range(1,101)]
+输出：
+['0001.peiqgmjxix',
+ '0002.nspwscivaz',
+ ...
+ '0099.gavzudepld',
+ '0100.fmpdsztsie']
+# 这同样是标准输出。
+
+import string
+['{:>04}.{}'.format(i,''.join(random.choice(string.ascii_lowercase) for _ in range(0,10))) for i in range(1,101)]
+输出：
+['0001.anbhqpafcz',
+ '0002.dkbzbypera',
+ ...
+ '0099.otxpmioqpp',
+ '0100.jazkayuqfu']
+# 这还是标准输出
+
+# 五、生成器表达式 Generator expression
+# - 语法
+#  - (返回值 for 元素 in 可迭代对象 if 条件)
+#   - 列表解析式的中括号换成小括号就行了
+#   - 返回一个生成器
+# - 和列表解析式的区别
+#   - 生成器表达式是按需计算(或称惰性求值、延迟计算),需要的时候才计算值
+#   - 列表解析式是立即返回值
+# 生成器表达式和列表解析式的对比
+# - 计算方式
+#   - 生成器表达式延迟计算,列表解析式立即计算
+# - 内存占用
+#   - 单从返回值本身来说,生成器表达式省内存,列表解析式返回新的列表
+#   - 生成器没有数据,内存占用极少,但是使用的时候,虽然一个个返回数据,但是合起来占用的内存也差不多
+#   - 列表解析式构造新的列表需要占用内存
+# - 计算速度
+#   - 单看计算时间看,生成器表达式耗时非常短,列表解析式耗时长
+#   - 但是生成器本身并没有返回任何值,只返回了一个生成器对象
+#   - 列表解析式构造并返回了一个新的列表
+
+
+
+
+
+
+
+
+
+
+
+
+# 六、集合解析式
+# - 语法
+#   - {返回值 for 元素 in 可迭代对象 if 条件}
+#   - 列表解析式的中括号换成大括号{}就行了
+#   - 立即返回一个集合
+# - 用法
+#   - {(x,x+1) for x in range(10)}
+#   - {[x] for x in range(10)}
+
+
+
+# 七、### 字典解析式
+# - 语法
+#   - {返回值 for 元素 in 可迭代对象 if 条件}
+#   - 列表解析式的中括号换成大括号{}就行了
+#   - 使用key:value形式
+#   - 立即返回一个字典
+# - 用法
+#   - {x:(x,x+1) for x in range(10)}
+#   - {x:[x,x+1] for x in range(10)}
+#   - {(x,):[x,x+1] for x in range(10)}
+#   - {[x]:[x,x+1] for x in range(10)} #
+#   - {chr(0x41+x):x**2 for x in range(10)}
+#   - {str(x):y for x in range(3) for y in range(4)} # 输出多少个元素?
+
+
+
+
+
+
